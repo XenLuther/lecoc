6cbfa89bd78ff45dde674aaee246f36a537975a5
XenLuther <101648861+XenLuther@users.noreply.github.com>
2022-03-15 15:00:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include 'conn.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DB Lecoc</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link href="main.css" rel="stylesheet">
+<body>
+
+<div id="side1">
+    <a href="select.php">The Select Page</a>
+</div>
+<div id="side2">
+    <a href="insert.php">The Insert Page</a>
+    <a href="lists.php">The Lists Page</a>
+</div>
+
+</body>
+</html>
